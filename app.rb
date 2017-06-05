3f4c001d48e73cc9bca8c9bf41eab9d09e41b307
yungpanko <jrjohns@mit.edu>
2017-06-05 16:52:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Jared"
+  end
+
+  get '/hometown' do
+  "My hometown is St. Louis"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Lens by Frank Ocean"
+  end
+  
 end
